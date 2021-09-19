548f32dd66d4612be1b3117bd266b929359a4303
SoummerELMehdi <esoummer@insea.ac.ma>
2021-09-19 23:21:31 +0100

    train

--- model.py
+++ model.py
@@ -161,3 +161,31 @@ def evaluate(model, val_dataloader):
     val_accuracy = np.mean(val_accuracy)
 
     return val_loss, val_accuracy
+
+def bert_predict(model, test_dataloader):
+    """Perform a forward pass on the trained BERT model to predict probabilities
+    on the test set.
+    """
+    # Put the model into the evaluation mode. The dropout layers are disabled during
+    # the test time.
+    model.eval()
+
+    all_logits = []
+
+    # For each batch in our test set...
+    for batch in test_dataloader:
+        # Load batch to GPU
+        b_input_ids, b_attn_mask = tuple(t.to(device) for t in batch)[:2]
+
+        # Compute logits
+        with torch.no_grad():
+            logits = model(b_input_ids, b_attn_mask)
+        all_logits.append(logits)
+    
+    # Concatenate logits from each batch
+    all_logits = torch.cat(all_logits, dim=0)
+
+    # Apply softmax to calculate probabilities
+    probs = F.softmax(all_logits, dim=1).cpu().numpy()
+
+    return probs
---
+++ train.py
@@ -0,0 +1,6 @@
+set_seed(42)    # Set seed for reproducibility
+bert_classifier, optimizer, scheduler = initialize_model(epochs=3)
+train(bert_classifier, train_dataloader, val_dataloader, epochs=3, evaluation=True)
+
+filename = 'trained_model.sav'
+pickle.dump(bert_classifier, open(filename, 'wb'))
