804d8d8ce325db5e2a72329fd8bbd64c856f4635
SoummerELMehdi <esoummer@insea.ac.ma>
2021-09-19 23:32:33 +0100

    test

---
+++ test.py
@@ -0,0 +1,22 @@
+probs = bert_predict(bert_classifier, val_dataloader)
+
+# Evaluate the Bert classifier
+evaluate_roc(probs, Y_val.astype(float))
+
+def evaluate_roc(probs, y_true):
+    """
+    - Print AUC and accuracy on the test set
+    - Plot ROC
+    @params    probs (np.array): an array of predicted probabilities with shape (len(y_true), 2)
+    @params    y_true (np.array): an array of the true values with shape (len(y_true),)
+    """
+    preds = probs[:, 1]
+    fpr, tpr, threshold = roc_curve(y_true, preds)
+    roc_auc = auc(fpr, tpr)
+    print(f'AUC: {roc_auc:.4f}')
+       
+    # Get accuracy over the test set
+    y_pred = np.where(preds >= 0.5, 1, 0)
+    accuracy = accuracy_score(y_true, y_pred)
+    print(f'Accuracy: {accuracy*100:.2f}%')
+  
